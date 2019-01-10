67f364f4fed6f5e86d8f78a5c657bac3f23cb779
julien <julienbouvet78@hotmail.com>
2019-01-10 14:48:00 +0100

    gestion log et des erreurs

---
+++ start-example.js
@@ -0,0 +1,7 @@
+var PHjs = require("./PHjs");
+
+var options = {};
+
+var libs = {};
+
+PHjs("/projects/PHjs/website/","http",8000,options,libs,"/projects/PHjs/access.log","/projects/PHjs/error.log","/projects/PHjs/config.txt");
